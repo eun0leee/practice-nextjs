2c72d7ebd4d3d07726c2c89e52a345432a9f23de
eun0leee <eun0leee@gmail.com>
2023-11-20 08:22:08 +0900

    feat : 예보 api 적용

---
+++ src/app/detail/[location]/page.tsx
@@ -0,0 +1,31 @@
+import HomeButton from '@/components/HomeButton';
+import { PramsProps } from '@/type/common';
+import { getForeCast } from '@/utils/getForecast';
+
+export default async function Detail({ params }: PramsProps) {
+  const res = await getForeCast(params.location);
+
+  return (
+    <div>
+      <h1>
+        {CITY_NAME.find((city) => city.location === params.location)?.name}의
+        3일 예보
+      </h1>
+      <ul>
+        {res.forecast.forecastday.map((day) => (
+          <li key={day.date}>
+            날짜 : {day.date}, 최고기온 : {day.day.maxtemp_c}, 최저기온 :{' '}
+            {day.day.mintemp_c}
+          </li>
+        ))}
+      </ul>
+      <HomeButton />
+    </div>
+  );
+}
+
+const CITY_NAME = [
+  { name: '서울', location: 'seoul' },
+  { name: '뉴욕', location: 'newyork' },
+  { name: '런던', location: 'london' },
+];
