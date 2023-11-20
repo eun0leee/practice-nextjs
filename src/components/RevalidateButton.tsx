f86355cbb26321e13253ccbf49696110d1a4f621
eun0leee <eun0leee@gmail.com>
2023-11-20 10:50:55 +0900

    feat : 데이터 재검증
    - 서버 api 생성

--- src/components/RevalidateButton.tsx
+++ src/components/RevalidateButton.tsx
@@ -1,22 +1,17 @@
 'use client';
 
-import { POST } from '@/api/revalidate/route';
-
 type Props = {
   tag: string;
 };
 
 export default function RevalidateButton({ tag }: Props) {
   const handleClick = async () => {
-    const res = await fetch('/api/revalidate?tag=' + tag, { method: 'POST' });
-    console.log(res);
-    const result = await res.json();
-    return result;
+    const res = await fetch('/api/revalidate?tag=' + tag, {
+      method: 'POST',
+    });
+
+    return res.json();
   };
 
-  return (
-    <button type="button" onClick={handleClick}>
-      캐시비우기
-    </button>
-  );
+  return <button onClick={handleClick}>캐시 비우기</button>;
 }
