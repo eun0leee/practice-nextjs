4f421cf26bd62eb9ac36ab33746f673e474c16f3
eun0leee <eun0leee@gmail.com>
2023-11-20 09:37:33 +0900

    feat : 현재 날씨 api 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { PramsProps } from '@/type/common';
+import { getCurrentWeather } from '@/utils/getCurrentWeather';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home({ params }: PramsProps) {
+  const res = await getCurrentWeather('seoul');
+
   return (
     <main>
       <h1>메인 페이지</h1>
